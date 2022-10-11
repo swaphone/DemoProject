59dedcfaf054f91506f73548b6322804d893ba23
Admin <Admin@DESKTOP-Swapnil>
2022-10-11 10:56:17 +0530

    pushing the commands

---
+++ src/PomClasses/Pomclass.java
@@ -0,0 +1,11 @@
+package PomClasses;
+
+public class Pomclass
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("hii to all");
+		
+	}
+
+}
---
+++ src/TestClasses/TastClass.java
@@ -0,0 +1,11 @@
+package TestClasses;
+
+public class TastClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("namskar Maharashtra");
+		
+	}
+
+}
